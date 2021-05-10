fd46dc861a5ffdcc66a47df6e784846c4fe9824a
kimgyuree <kkl0219kkl@naver.com>
2021-05-10 13:38:55 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -9,11 +9,13 @@ const id = document.querySelector("#id"),
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if(psword.value !== confirmPsword.value) {
+        return alert("비밀번호가 일치하지 않습니다.");
+    }
     const req = {
         id: id.value,
         name: name.value,
         psword: psword.value,
-        confirmPsword: confirmPsword.value,
     };
 
     fetch("/register", {
